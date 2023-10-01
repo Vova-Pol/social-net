641bdbcc207ab8d3dec338339fbdbeb273beeea7
Vova Pol <ya-chai777@yandex.ru>
2023-10-01 17:31:29 +0300

    Подключил БД H2

--- src/main/java/com/vladimir/socialnetrestapi/socialnet/user/User.java
+++ src/main/java/com/vladimir/socialnetrestapi/socialnet/user/User.java
@@ -1,12 +1,18 @@
 package com.vladimir.socialnetrestapi.socialnet.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
-
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should be at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id,birth_date,name,password)
+values(10001, current_date(), 'Vladimir', '1234');
+
+insert into user_details(id,birth_date,name,password)
+values(10002, current_date(), 'Josh', 'qwerty');
+
+insert into user_details(id,birth_date,name,password)
+values(10003, current_date(), 'Alex', 'my-password');
